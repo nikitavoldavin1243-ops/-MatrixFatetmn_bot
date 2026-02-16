3982faf04c49ac2cfb6a9f4a9d989c0df17ca487
nikitavoldavin1243-ops <nikitavoldavin1243@gmail.com>
2026-02-16 16:31:32 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,6 +9,16 @@ from telegram.ext import Application, CommandHandler, ContextTypes
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет!")
+import os
+from telegram.ext import Application
+
+# Получение токена из переменных окружения
+TOKEN = os.getenv("T8505345629:AAE_PrnTHpYI-yAhGuKPKFngtWhRWpt5SSA")
+if not TOKEN:
+    raise ValueError("Токен Telegram-бота не задан! Проверьте переменную TELEGRAM_BOT_TOKEN.")
+
+# Инициализация бота
+app = Application.builder().token(TOKEN).build()
 
 def main():
     app = Application.builder().token(8505345629:AAE_PrnTHpYI-yAhGuKPKFngtWhRWpt5SSA).build()
