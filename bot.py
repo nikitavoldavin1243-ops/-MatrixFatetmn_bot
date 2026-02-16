0435181e8e7885fecbab3fbe735a18b2d9fea258
nikitavoldavin1243-ops <nikitavoldavin1243@gmail.com>
2026-02-16 16:26:07 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,9 @@
+import logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+logging.info("Бот запущен")
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
@@ -5,6 +11,6 @@ async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет!")
 
 def main():
-    app = Application.builder().token(TOKEN).build()
+    app = Application.builder().token(8505345629:AAE_PrnTHpYI-yAhGuKPKFngtWhRWpt5SSA).build()
     app.add_handler(CommandHandler("start", start_command))
     app.run_polling()  # Теперь работает корректно
