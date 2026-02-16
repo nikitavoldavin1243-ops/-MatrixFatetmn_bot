a8b5ae177beba9ddf5c8ce1ce72df825e62b902e
nikitavoldavin1243-ops <nikitavoldavin1243@gmail.com>
2026-02-16 16:20:11 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,10 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = "8505345629:AAE_PrnTHpYI-yAhGuKPKFngtWhRWpt5SSA"
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет! Я работаю на Python 3.11 и PTB 20.7")
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Привет!")
 
 def main():
-    app = ApplicationBuilder().token(TOKEN).build()
-
-    app.add_handler(CommandHandler("start", start))
-
-    print("Бот запущен...")
-    app.run_polling()
-
-if __name__ == "__main__":
-    main()
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start_command))
+    app.run_polling()  # Теперь работает корректно
